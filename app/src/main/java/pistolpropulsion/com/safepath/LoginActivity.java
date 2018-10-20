9936c5fad2473a949a5948aaa347576b38b96f82
acouvillon <auburn2678@gmail.com>
2018-10-20 18:10:36 -0400

    Connected login to other pages

--- app/src/main/java/pistolpropulsion/com/safepath/LoginActivity.java
+++ app/src/main/java/pistolpropulsion/com/safepath/LoginActivity.java
@@ -1,4 +1,5 @@
 package pistolpropulsion.com.safepath;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -23,6 +24,7 @@ public class LoginActivity extends AppCompatActivity {
         private Button login_button;
         private EditText email;
         private EditText password;
+        private Button create_acct_button;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -31,13 +33,23 @@ public class LoginActivity extends AppCompatActivity {
             email = findViewById(R.id.EmailInput);
             password = findViewById(R.id.PasswordInput);
             login_button = findViewById(R.id.LoginButton);
+            create_acct_button = findViewById(R.id.CreateAcct);
+
             login_button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     signIn(email.getText().toString(), password.getText().toString());
                 }
             });
+
+            create_acct_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    createAcct();
+                }
+            });
         }
+
         public void signIn(String email, String password) {
             siAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                 @Override
@@ -46,24 +58,34 @@ public class LoginActivity extends AppCompatActivity {
                         // Sign in success, update UI with the signed-in user's information
                         Log.d(TAG, "signInWithEmail:success");
                         FirebaseUser user = siAuth.getCurrentUser();
-                        updateUI(user);
+                        updateSignIn(user);
                     } else {
                         // If sign in fails, display a message to the user.
                         Log.w(TAG, "signInWithEmail:failure", task.getException());
                         Toast.makeText(LoginActivity.this, "Authentication failed.",
                                 Toast.LENGTH_SHORT).show();
-                        updateUI(null);
+                        updateSignIn(null);
                     }
                 }
             });
         }
+
+        public void createAcct() {
+            Intent createintent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(createintent);
+        }
+
         @Override
         public void onStart() {
             super.onStart();
             // Check if user is signed in (non-null) and update UI accordingly.
             FirebaseUser currentUser = siAuth.getCurrentUser();
-            updateUI(currentUser);
+            updateSignIn(currentUser);
         }
-        public void updateUI(FirebaseUser user) {
+        public void updateSignIn(FirebaseUser user) {
+            if (user != null) {
+                Intent signintent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(signintent);
+            }
         }
 }
