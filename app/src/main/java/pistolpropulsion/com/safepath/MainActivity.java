090aaaee6b7ed2d64158282ab462f52398b69a15
CallmeJoeBob <joel.joseprabu@gmail.com>
2018-10-20 01:14:05 -0400

    Integrated Awareness API

--- app/src/main/java/pistolpropulsion/com/safepath/MainActivity.java
+++ app/src/main/java/pistolpropulsion/com/safepath/MainActivity.java
@@ -1,7 +1,11 @@
 package pistolpropulsion.com.safepath;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.android.gms.awareness.Awareness;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android.content.Context context = getApplicationContext();
+        GoogleApiClient client = new GoogleApiClient.Builder(context)
+                .addApi(Awareness.API)
+                .build();
+        client.connect();
     }
 }
