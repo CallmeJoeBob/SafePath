8663fc5e7b51c1df267051be2c94a2f6ffc3deec
vab10266 <vaudalexburton@gmail.com>
2018-10-20 19:22:25 -0400

    stuff

--- app/src/main/java/pistolpropulsion/com/safepath/CreateAccountActivity.java
+++ app/src/main/java/pistolpropulsion/com/safepath/CreateAccountActivity.java
@@ -72,6 +72,7 @@ public class CreateAccountActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
+
                             updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
