98ee321bb9717cf6b92d244b1182f46125c92f88
CallmeJoeBob <joel.joseprabu@gmail.com>
2018-10-20 10:05:36 -0400

    Getting fences to kind of work

--- app/src/main/java/pistolpropulsion/com/safepath/MainActivity.java
+++ app/src/main/java/pistolpropulsion/com/safepath/MainActivity.java
@@ -1,23 +1,205 @@
 package pistolpropulsion.com.safepath;
 
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.awareness.Awareness;
+import com.google.android.gms.awareness.FenceApi;
+import com.google.android.gms.awareness.fence.AwarenessFence;
+import com.google.android.gms.awareness.fence.FenceQueryRequest;
+import com.google.android.gms.awareness.fence.FenceQueryResult;
+import com.google.android.gms.awareness.fence.FenceState;
+import com.google.android.gms.awareness.fence.FenceStateMap;
+import com.google.android.gms.awareness.fence.FenceUpdateRequest;
+import com.google.android.gms.awareness.fence.LocationFence;
+import com.google.android.gms.awareness.snapshot.LocationResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.ResultCallbacks;
+import com.google.android.gms.common.api.Status;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String FENCE_RECEIVER_ACTION = "FENCE_RECEIVE";
+
+    private LocationBroadcastReceiver fenceReceiver;
+    private PendingIntent mFencePendingIntent;
+    private GoogleApiClient mGoogleApiClient;
+    private static final String TAG = "MainActivity";
+
+    private TextView status;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        status = findViewById(R.id.status);
+        button = findViewById(R.id.button);
 
         android.content.Context context = getApplicationContext();
-        GoogleApiClient client = new GoogleApiClient.Builder(context)
+        mGoogleApiClient = new GoogleApiClient.Builder(context)
                 .addApi(Awareness.API)
                 .build();
-        client.connect();
+        mGoogleApiClient.connect();
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_CALENDAR)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    0);
+        }
+
+        Awareness.SnapshotApi.getLocation(mGoogleApiClient)
+                .setResultCallback(new ResultCallback<LocationResult>() {
+                    @Override
+                    public void onResult(@NonNull LocationResult locationResult) {
+                        if (!locationResult.getStatus().isSuccess()) {
+                            Log.e(TAG, "Could not get location.");
+                            return;
+                        }
+                        Location location = locationResult.getLocation();
+                        Log.i(TAG, "Lat: " + location.getLatitude() + ", Lon: " + location.getLongitude());
+                    }
+                });
+
+
+        fenceReceiver = new LocationBroadcastReceiver();
+        Intent intent = new Intent(FENCE_RECEIVER_ACTION);
+        mFencePendingIntent = PendingIntent.getBroadcast(MainActivity.this,
+                10001,
+                intent,
+                0);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    protected void registerFence() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_CALENDAR)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    0);
+        }
+        AwarenessFence locationFence = AwarenessFence.not(LocationFence.in(33.776569, -84.3960469, 10, 100));
+        Awareness.FenceApi.updateFences(
+                mGoogleApiClient,
+                new FenceUpdateRequest.Builder()
+                        .addFence("loc", locationFence, mFencePendingIntent)
+                        .build())
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        if (status.isSuccess()) {
+                            Log.i(TAG, "Fence was successfully registered.");
+                        } else {
+                            Log.e(TAG, "Fence could not be registered: " + status);
+                        }
+                    }
+                });
+    }
+
+    protected void unregisterFence() {
+        Awareness.FenceApi.updateFences(
+                mGoogleApiClient,
+                new FenceUpdateRequest.Builder()
+                        .removeFence("loc")
+                        .build()).setResultCallback(new ResultCallbacks<Status>() {
+            @Override
+            public void onSuccess(@NonNull Status status) {
+                Log.i(TAG, "Fence " + "loc" + " successfully removed.");
+            }
+
+            @Override
+            public void onFailure(@NonNull Status status) {
+                Log.i(TAG, "Fence " + "loc" + " could NOT be removed.");
+            }
+        });
+    }
+
+
+    class LocationBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            FenceState fenceState = FenceState.extract(intent);
+
+            Log.d(TAG, "Fence Receiver Received");
+
+            if (TextUtils.equals(fenceState.getFenceKey(), "loc")) {
+                switch (fenceState.getCurrentState()) {
+                    case FenceState.TRUE:
+                        status.setText("You left.");
+                        break;
+                    case FenceState.FALSE:
+                        status.setText("You didn't leave.");
+                        break;
+                    case FenceState.UNKNOWN:
+                        status.setText("Idk bro.");
+                        break;
+                }
+            }
+        }
+
+    }
+
+    protected void queryFence(final String fenceKey) {
+        Awareness.FenceApi.queryFences(mGoogleApiClient,
+                FenceQueryRequest.forFences(Arrays.asList(fenceKey)))
+                .setResultCallback(new ResultCallback<FenceQueryResult>() {
+                    @Override
+                    public void onResult(@NonNull FenceQueryResult fenceQueryResult) {
+                        if (!fenceQueryResult.getStatus().isSuccess()) {
+                            Log.e(TAG, "Could not query fence: " + fenceKey);
+                            return;
+                        }
+                        FenceStateMap map = fenceQueryResult.getFenceStateMap();
+                        for (String fenceKey : map.getFenceKeys()) {
+                            FenceState fenceState = map.getFenceState(fenceKey);
+                            Log.i(TAG, "Fence " + fenceKey + ": "
+                                    + fenceState.getCurrentState()
+                                    + ", was="
+                                    + fenceState.getPreviousState()
+                                    + ", lastUpdateTime=");
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerFence();
+        registerReceiver(fenceReceiver, new IntentFilter(FENCE_RECEIVER_ACTION));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterFence();
+        unregisterReceiver(fenceReceiver);
     }
 }
