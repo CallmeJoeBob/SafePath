8e9489323e33c7cda74b180abcdb29361db0525f
vab10266 <vaudalexburton@gmail.com>
2018-10-20 16:26:35 -0400

    Revert "Merge branch 'master' of https://github.com/CallmeJoeBob/SafePath"
    This reverts commit 85831cda64c2cde3e86f203e0115d2fa8bd808a5, reversing
    changes made to 39ce04873224f2b42ebc4d088edfc0c03a7b1a0d.

--- app/src/main/java/pistolpropulsion/com/safepath/LoginActivity.java
+++ app/src/main/java/pistolpropulsion/com/safepath/LoginActivity.java
@@ -1,83 +1,19 @@
 package pistolpropulsion.com.safepath;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import static android.content.ContentValues.TAG;
 
 /**
  * Created by Abby on 10/20/18.
  */
 
 public class LoginActivity extends AppCompatActivity {
+    public class CreateAccountActivity extends AppCompatActivity {
 
-    private FirebaseAuth siAuth;
-    private Button login_button;
-    private EditText email;
-    private EditText password;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        siAuth = FirebaseAuth.getInstance();
-
-        email = findViewById(R.id.EmailInput);
-        password = findViewById(R.id.PasswordInput);
-        login_button = findViewById(R.id.LoginButton);
-
-        login_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signIn(email.getText().toString(), password.getText().toString());
-            }
-        });
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login);
+        }
     }
-
-    public void signIn(String email, String password) {
-        siAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithEmail:success");
-                    FirebaseUser user = siAuth.getCurrentUser();
-                    updateUI(user);
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithEmail:failure", task.getException());
-                    Toast.makeText(LoginActivity.this, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show();
-                    updateUI(null);
-                }
-            }
-        });
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = siAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
-
-    public void updateUI(FirebaseUser user) {
-
-    }
-
 }
