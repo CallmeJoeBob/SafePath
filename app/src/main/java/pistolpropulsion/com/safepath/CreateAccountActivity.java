4831a6b9092cf634b1fcac0af9b8ebc6b86afbbb
acouvillon <auburn2678@gmail.com>
2018-10-20 13:07:35 -0400

    Login and Create Acct Screens

---
+++ app/src/main/java/pistolpropulsion/com/safepath/CreateAccountActivity.java
@@ -0,0 +1,19 @@
+package pistolpropulsion.com.safepath;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Abby on 10/20/18.
+ */
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert);
+
+
+    }
+}
